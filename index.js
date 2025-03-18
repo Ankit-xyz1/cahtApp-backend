4577560a9303a9b40aa33f66e997d77b926d7fd9
Ankit <86563541+Ankit-xyz1@users.noreply.github.com>
2025-03-18 18:31:51 +0530

    Update index.js
    cors fix

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+    'https://chat-app-red-rho.vercel.app',
+    'https://caht-app-backend.vercel.app'
+];
+
 app.use(cors({
-    credentials:true,
-    origin:"https://chat-app-red-rho.vercel.app",
-}))
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true // Allows cookies & authentication
+}));
 
 const __dirname = path.resolve();
 
