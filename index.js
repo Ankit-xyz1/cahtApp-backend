fec7754af49a6cc9c778ae9f11f91f7cfd824ddb
Ankit <86563541+Ankit-xyz1@users.noreply.github.com>
2025-03-25 13:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const allowedOrigins = ['https://chat-app-red-rho.vercel.app']; // Your frontend domain
 
 app.use(cors({
-    origin: "https://chat-app-red-rho.vercel.app/",
+    origin: "https://chat-app-red-rho.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
